825aed0a8e148ce937a22fb91130510f19a3469a
Xiro Jotro <rixmaestro@gmail.com>
2016-10-09 22:51:27 +0800

    added python file and list of nodes for tagging

---
+++ python/tags.py
@@ -0,0 +1,17 @@
+import mysql.connector as sql
+
+conn = sql.connect(user='rese2nse', password='rese2nse', database='graph')
+conn.autocommit = True
+c = conn.cursor()
+
+nlist = open("python/node_position.txt", "r")
+
+for line in nlist:
+	fields = line.strip().split(',')
+	mac = int(fields[0], 16)
+	query = "INSERT INTO tags VALUES(%d,'%s')" % (mac,fields[1])
+	c.execute(query)
+
+conn.commit()
+
+nlist.close()
