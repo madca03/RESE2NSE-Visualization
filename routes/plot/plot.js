91f12a890019d26329a3663ee86ce8cd2820388a
Xiro Jotro <rixmaestro@gmail.com>
2016-10-09 15:15:41 +0800

    Edite files to support selection of groups in frontend and some on backend

--- routes/plot/plot.js
+++ routes/plot/plot.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function(req, res) {
-  res.render('plot', {title: 'Plot', resolution: '10 mins'});
-});
+var the_res = {"10mins":"10 mins", "1hr":"1 hr", "3hrs":"3 hrs", "6hrs":"6 hrs", "12hrs":"12 hrs", "1day":"1 day"};
 
+router.get('/:node_group/:time_res', function(req, res) {
+  res.render('plot', {title: 'Plot', resolution: the_res[req.params["time_res"]], group: req.params["node_group"]});
+});
+/*
 router.get('/1hr', function(req, res) {
   res.render('plot', {title: 'Plot', resolution: '1 hr'});
 });
@@ -24,5 +26,5 @@ router.get('/12hrs', function(req, res) {
 router.get('/1day', function(req, res) {
   res.render('plot', {title: 'Plot', resolution: '1 day'});
 });
-
+*/
 module.exports = router;
