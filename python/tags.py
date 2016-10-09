9a9493e671e5a5944773ad5e9c71d1e98d53adc4
Xiro Jotro <rixmaestro@gmail.com>
2016-10-10 01:51:44 +0800

    added power panel nodes in list of tagged nodes, ensures tags.py won't add existing tags

--- python/tags.py
+++ python/tags.py
@@ -9,8 +9,12 @@ nlist = open("python/node_position.txt", "r")
 for line in nlist:
 	fields = line.strip().split(',')
 	mac = int(fields[0], 16)
-	query = "INSERT INTO tags VALUES('%d','%s')" % (mac,fields[1])
+	query = "SELECT COUNT(*) FROM tags WHERE src = '%s' AND tag = '%s'" % (mac, fields[1])
 	c.execute(query)
+	res_cnt = c.fetchone()[0]
+	if res_cnt == 0:
+		query = "INSERT INTO tags VALUES('%d','%s')" % (mac,fields[1])
+		c.execute(query)
 
 conn.commit()
 
